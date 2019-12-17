81a2ec79887e3d7ba3d0d9682d667511f752d10e
Natalya Myunster <natalyamyunster@yahoo.com>
2019-12-17 16:08:35 +0100

    method borrowing - apply()

--- call_and_apply_methods.js
+++ call_and_apply_methods.js
@@ -21,4 +21,11 @@ showFullName1.call(user); //Natalya  Myunster
 showFullName.call(user,'firstName', 'lastName'); //Natalya  Myunster
 showFullName.call(user, 'firstName', 'patronym'); //Natalya  Viktorovna
 
-//«Одалживание метода»
+//«Одалживание метода» METHOD BORROWING
+//При помощи call можно легко взять метод одного объекта,
+// в том числе встроенного, и вызвать в контексте другого.
+
+//arguments не массив, а обычный объект,
+// поэтому таких полезных методов как push, pop, join и других у него нет.
+
+
