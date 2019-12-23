b9f73e27aa2e1821c2af397d036077298a242d23
Natalya Myunster <natalyamyunster@yahoo.com>
2019-12-23 08:56:00 +0100

    riassunto.js

--- Riassunto.js
+++ Riassunto.js
@@ -1,2 +1,16 @@
 //Итого про this
 
+//Значение this устанавливается в зависимости от того, как вызвана функция:
+// При вызове функции как метода:
+
+obj.func(...)    // this = obj
+obj["func"](...)
+//При обычном вызове:
+
+    func(...) // this = window (ES3) /undefined (ES5)
+//В new:
+
+new func() // this = {} (новый объект)
+//Явное указание:
+func.call(context, arg1, arg2, ...)
+func.apply(context, args) // this = context (явная передача)
