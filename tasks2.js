f26cb012461fa382b188cc2928524eba05cbd9db
Natalya Myunster <natalyamyunster@yahoo.com>
2019-12-26 17:25:21 +0100

    tasks2.js applyAll function

--- tasks2.js
+++ tasks2.js
@@ -27,3 +27,33 @@ function sumArg() {
 }
 
 console.log( sumArg(1, 2, 3) ); //6
+
+//2. Примените функцию к аргументам
+// важность: 5
+// Напишите функцию applyAll(func, arg1, arg2...), которая получает функцию func и произвольное количество аргументов.
+//
+// Она должна вызвать func(arg1, arg2...), то есть передать в func все аргументы, начиная со второго, и возвратить результат.
+/// Применить Math.max к аргументам 2, -2, 3
+console.log( applyAll(Math.max, 2, -2, 3) ); // 3
+//
+// // Применить Math.min к аргументам 2, -2, 3
+console.log( applyAll(Math.min, 2, -2, 3) ); // -2
+
+function sum1() { // суммирует аргументы: sum(1,2,3) = 6
+    return [].reduce.call(arguments, function(a, b) {
+        return a + b;
+    });
+}
+
+function mul() { // перемножает аргументы: mul(2,3,4) = 24
+    return [].reduce.call(arguments, function(a, b) {
+        return a * b;
+    });
+}
+
+function applyAll(func) {
+    return func.apply(this, [].slice.call(arguments, 1));
+}
+
+console.log( applyAll(sum1, 1, 2, 3) ); // -> sum(1, 2, 3) = 6
+console.log( applyAll(mul, 2, 3, 4) ); // -> mul(2, 3, 4) = 24
