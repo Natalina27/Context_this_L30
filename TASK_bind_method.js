103484e2f258f60770b16acca8585d38a1fc8362
Natalya Myunster <natalyamyunster@yahoo.com>
2020-01-19 20:05:57 +0100

    first task of bind method

---
+++ TASK_bind_method.js
@@ -0,0 +1,12 @@
+//1.Связанная функция как метод
+//Что выведет функция?
+
+    function f() {
+        console.log( this ); // ?
+    }
+
+    let user = {
+    g: f.bind(null),
+        };
+
+user.g();
